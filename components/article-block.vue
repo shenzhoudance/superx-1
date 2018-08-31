83312aa703098b8aee687fade92a442663e61be9
Coder-LG <1203732857@qq.com>
2018-08-31 19:31:47 +0800

    ｍｄ

--- components/article-block.vue
+++
@@ -1,9 +0,0 @@
-<!-- my-component.vue -->
-<template>
-  <div></div>
-</template>
-<script>
-
-</script>
-<style src="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css"></style>
-<style src="../css/main.css"></style>
