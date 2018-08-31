0ad6be373a3a10dccf92ffa70dfdd702694bf9a9
Coder-LG <1203732857@qq.com>
2018-08-31 15:51:47 +0800

    first commit

---
+++ components/article-block.vue
@@ -0,0 +1,9 @@
+<!-- my-component.vue -->
+<template>
+  <div></div>
+</template>
+<script>
+
+</script>
+<style src="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css"></style>
+<style src="../css/main.css"></style>
